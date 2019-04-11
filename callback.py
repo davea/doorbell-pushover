afb9153e7e6fec0d739745932c638b9c0eda4a65
Dave Arter <github@davea.me>
2019-04-11 16:28:44 +0100

    Include webcam photo if available

--- callback.py
+++ callback.py
@@ -5,9 +5,17 @@ import requests
 
 def callback(topic, payload):
     if payload == b"ON":
+        files = None
+        if os.environ.get("DOORBELL_CAMERA_URL"):
+            try:
+                files = {
+                    "attachment": ("image.jpeg", requests.get(os.environ['DOORBELL_CAMERA_URL'], stream=True, timeout=5).raw)
+                }
+            except:
+                pass
         requests.post("https://api.pushover.net/1/messages.json", {
             'token': os.environ['PUSHOVER_APP_TOKEN'],
             'user': os.environ['PUSHOVER_USER_TOKEN'],
             'message': os.environ.get("PUSHOVER_MESSAGE", "Someone is at the door!"),
             'sound': os.environ.get("PUSHOVER_SOUND", "persistent"),
-        })
+        }, files=files, timeout=5)
