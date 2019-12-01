0a704975ce438ba3bed601de487f4f2579754c36
Dave Arter <github@davea.me>
2019-12-01 20:23:23 +0000

    Allow photos in binary MQTT messages to be sent via Pushover

--- callback.py
+++ callback.py
@@ -4,18 +4,24 @@ import os
 import requests
 
 def callback(topic, payload):
-    if payload == b"ON":
-        files = None
-        if os.environ.get("DOORBELL_CAMERA_URL"):
-            try:
-                files = {
-                    "attachment": ("image.jpeg", requests.get(os.environ['DOORBELL_CAMERA_URL'], stream=True, timeout=5).raw)
-                }
-            except:
-                pass
-        requests.post("https://api.pushover.net/1/messages.json", {
-            'token': os.environ['PUSHOVER_APP_TOKEN'],
-            'user': os.environ['PUSHOVER_USER_TOKEN'],
-            'message': os.environ.get("PUSHOVER_MESSAGE", "Someone is at the door!"),
-            'sound': os.environ.get("PUSHOVER_SOUND", "persistent"),
-        }, files=files, timeout=5)
+    binary_topics = set(os.getenv("PUSHOVER_BINARY_TOPICS", "").split(","))
+    if not (payload == b"ON" or topic in binary_topics):
+        return
+    files = None
+    if topic in binary_topics:
+        files = {
+            "attachment": ("image.jpeg", payload)
+        }
+    elif os.environ.get("DOORBELL_CAMERA_URL"):
+        try:
+            files = {
+                "attachment": ("image.jpeg", requests.get(os.environ['DOORBELL_CAMERA_URL'], stream=True, timeout=5).raw)
+            }
+        except:
+            pass
+    requests.post("https://api.pushover.net/1/messages.json", {
+        'token': os.environ['PUSHOVER_APP_TOKEN'],
+        'user': os.environ['PUSHOVER_USER_TOKEN'],
+        'message': os.environ.get("PUSHOVER_MESSAGE", "Someone is at the door!"),
+        'sound': os.environ.get("PUSHOVER_SOUND", "persistent"),
+    }, files=files, timeout=5)
